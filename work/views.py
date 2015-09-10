ab33a6c83497758856b42ebc9cd539f97f1ac7c1
gqdw <aca_jingru@qq.com>
2015-09-10 11:28:04 +0800

    Changes to be committed: 	modified:   .gitignore 	modified:   work/views.py django can pass the val to main.py

--- work/views.py
+++ work/views.py
@@ -1,18 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django import forms
+from main import MysqlInfo
 # Create your views here.
 
 class MysqlForm( forms.Form):
-	ip = forms.GenericIPAddressField()
+	host = forms.GenericIPAddressField()
 	user = forms.CharField(max_length=30)
-	password = forms.CharField(max_length=60)
+	password = forms.CharField(max_length=60,initial='')
+	dbname = forms.CharField(max_length=30)
 
 def index( request):
 	if request.method == 'POST':
 		form = MysqlForm(request.POST)
 		if form.is_valid():
 			cd = form.cleaned_data
-			print cd['ip'], cd['user'] ,cd['password']
+			print cd['host'], cd['user'] ,cd['password']
+			n1 = MysqlInfo( cd['user'],cd['password'] ,cd['host'],cd['dbname'])
+			n1.get_mysql_info()
+			n1.write_to_excel()
+			return HttpResponse('ok')
 
 	else:
 		form = MysqlForm()
